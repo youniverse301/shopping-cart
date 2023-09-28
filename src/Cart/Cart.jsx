9877dcb628798fe23a0a509bf32468a33c12f296
Bjorn <bjorneng3@gmail.com>
2023-09-27 23:35:45 -0400

    Skeleton products no css added

--- src/Cart/Cart.jsx
+++ src/Cart/Cart.jsx
@@ -2,6 +2,38 @@ import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
 import Header from "../Header/Header";
 
+function CartData () {
+    const cartData = {
+        "cart": [
+          {
+            "id": 1,
+            "name": "Product 1",
+            "price": 10.99,
+            "quantity": 2
+          },
+          {
+            "id": 2,
+            "name": "Product 2",
+            "price": 19.99,
+            "quantity": 1
+          }
+        ]
+      };
+      console.log(cartData)
+    
+      function removeFromCart(cart, itemId) {
+        const updatedCart = cart.cart.filter(item => item.id !== itemId);
+        return { "cart": updatedCart };
+      }
+      
+      // Remove item with ID 2 from the cart
+      const updatedCartData = removeFromCart(cartData, 2);
+      
+      console.log(updatedCartData);
+    
+    }
+    CartData();
+
 const Cart = () => {
 
     return(
